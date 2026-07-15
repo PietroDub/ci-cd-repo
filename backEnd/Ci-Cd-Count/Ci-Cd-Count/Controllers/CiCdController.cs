dbb14fe18b9a9c0f795ff04c317b68a73f5771ab
pietrodub <pietroti453@gmail.com>
2026-07-15 08:59:40 -0300

    initial commit ci cd pipeline repository

---
+++ backEnd/Ci-Cd-Count/Ci-Cd-Count/Controllers/CiCdController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ci_Cd_Count.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CiCdController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Message()
+        {
+            return Ok(new
+            {
+                message = "Olá GitHub Actions!"
+            });
+        }
+    }
+}
---
+++ backEnd/Ci-Cd-Count/Ci-Cd-Count/Program.cs
@@ -0,0 +1,42 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("FrontEnd", policy =>
+    {
+        policy.WithOrigins(
+           "http://localhost:3000",
+           "http://127.0.0.1:3000"
+        )
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("FrontEnd");
+
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
